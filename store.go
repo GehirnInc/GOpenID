9787fb93a0e9ea9418eae528fa9bbd38ab2ec453
Kohei YOSHIDA <kohei.yoshida@gehirn.co.jp>
2014-01-21 16:02:01 +0900

    add methods for nonce

--- store.go
+++ store.go
@@ -4,4 +4,6 @@ type Store interface {
 	StoreAssociation(*Association) error
 	GetAssociation(string, bool) (*Association, error)
 	DeleteAssociation(*Association) error
+	IsKnownNonce(nonce string) (bool, error)
+	StoreNonce(nonce string) error
 }
