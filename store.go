5628b6ae410eee7e28f487bb56921fc44f885de2
Kohei YOSHIDA <kohei.yoshida@gehirn.co.jp>
2014-01-21 20:26:23 +0900

    delete name of arguments

--- store.go
+++ store.go
@@ -4,6 +4,6 @@ type Store interface {
 	StoreAssociation(*Association) error
 	GetAssociation(string, bool) (*Association, error)
 	DeleteAssociation(*Association) error
-	IsKnownNonce(nonce string) (bool, error)
-	StoreNonce(nonce string) error
+	IsKnownNonce(string) (bool, error)
+	StoreNonce(string) error
 }
